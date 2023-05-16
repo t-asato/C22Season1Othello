57cbd5a672680360ba3256affc266b2aa17fc893
teruya <c22015@std.it-college.ac.jp>
2023-05-16 17:02:26 +0900

    のすけだよ！

---
+++ Assets/Scripts/Nosuke/IMGLoadScript_Nosuke.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IMGLoadScript_Nosuke : MonoBehaviour
+{
+    public CSVRead_Nosuke csvreader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
